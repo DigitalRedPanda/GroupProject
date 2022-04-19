e76985aa9a513301527c10369d72d42e97364d9e
DigitalRedPanda <osama-12341905@hotmail.com>
2022-04-20 02:01:36 +0300

    Creating the Registeration interface

---
+++ src/company/member/CEO.java
@@ -0,0 +1,11 @@
+package company.member;
+
+public class CEO extends Member {
+
+ public CEO() {
+
+  super();
+
+ }
+
+}
---
+++ src/company/member/Member.java
@@ -0,0 +1,55 @@
+package company.member;
+
+import java.util.Date;
+
+import java.util.function.Predicate;
+
+import java.util.regex.Pattern;
+
+public abstract class Member {
+
+    private String email;
+
+    private String password;
+
+    private Date date;
+
+    public Member() {
+
+        date = new Date();
+
+    }
+
+    public Member(String email, String password) {
+
+        this();
+
+        this.email = email;
+
+        this.password = password;
+
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Don't modify this
+    public static Predicate<String> validateEmail = email -> {
+        return Pattern.compile("^[^@][\\w[^@]]+(@[a-z]+\\.)(com|net|(edu|co)\\.[a-z]{2})(/s)*$").matcher(email)
+                .matches();
+    };
+
+}
