93d57d28d21ee01fc3919ac7412d8d63589cd72f
DigitalRedPanda <osama-12341905@hotmail.com>
2022-04-20 23:29:11 +0300

    More implementations

--- src/util/gui/GUI.java
+++ src/util/gui/GUI.java
@@ -1,4 +1,13 @@
 package util.gui;
 
-public class GUI {
+import javafx.application.Application;
+
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+ @Override
+ public void start(Stage arg0) throws Exception {
+
+ }
 }
