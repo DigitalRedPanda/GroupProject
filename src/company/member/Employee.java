8625e855b30cf980d3fc4d659ffe34b792da02bd
DigitalRedPanda <osama-12341905@hotmail.com>
2022-04-20 19:27:57 +0300

    Creating the Registeration interface

---
+++ src/company/member/Employee.java
@@ -0,0 +1,12 @@
+package company.member;
+
+import java.util.Optional;
+
+public class Employee extends Member {
+
+ @Override
+ public Optional<? extends Member> verify() {
+  return null;
+ }
+
+}
