9b8ed7d55820555a8a823599e742ea21642854b0
DigitalRedPanda <osama-12341905@hotmail.com>
2022-04-24 01:49:32 +0300

    Optimizing the Member and Employee classes

--- src/company/member/Employee.java
+++ src/company/member/Employee.java
@@ -6,19 +6,16 @@ public class Employee extends Member implements Account {
 
  @Override
  public void signIn() {
-  // TODO Auto-generated method stub
 
  }
 
  @Override
  public void register() {
-  // TODO Auto-generated method stub
 
  }
 
  @Override
  public boolean isStored() {
-  // TODO Auto-generated method stub
   return false;
  }
 
