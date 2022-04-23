d0dda9216d6100052ee93d151eb03ae022b9ec6b
DigitalRedPanda <osama-12341905@hotmail.com>
2022-04-23 13:17:40 +0300

    Improving GUI

--- src/util/gui/GUI.java
+++ src/util/gui/GUI.java
@@ -1,38 +1,19 @@
 package util.gui;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.stream.Stream;
-
 import javafx.animation.TranslateTransition;
-
 import javafx.application.Application;
-
-import javafx.geometry.Insets;
-
 import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-
 import javafx.scene.control.TextField;
-
 import javafx.scene.image.Image;
-
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-
 import javafx.scene.paint.Color;
-
 import javafx.scene.text.Text;
-
 import javafx.stage.Screen;
-
 import javafx.stage.Stage;
-
 import javafx.stage.StageStyle;
 
 public class GUI extends Application {
