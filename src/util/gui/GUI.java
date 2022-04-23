f4fab74f52885818cddee52e56d6606a4c08cf9c
DigitalRedPanda <osama-12341905@hotmail.com>
2022-04-24 00:34:08 +0300

    Improving GUI

--- src/util/gui/GUI.java
+++ src/util/gui/GUI.java
@@ -1,6 +1,7 @@
 package util.gui;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -18,22 +19,20 @@ public class GUI extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     // Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
-
     VBox loginLayout = new VBox(5);
     TextField txtf1 = new TextField();
     txtf1.setPromptText("Enter email");
     PasswordField txtf2 = new PasswordField();
     txtf2.setPromptText("Enter password");
     Scene loginScene = new ModifiedScene(loginLayout, new Text("Login"), new TextField[] { txtf1, txtf2 });
-    loginScene.setFill(Color.valueOf("#142850"));
+    // loginScene.setFill(Color.valueOf("#142850"));
     Button login = new Button();
     login.setText("Login");
     login.setStyle("-fx-color: #4e4b44; -fx-font: 15 \"Monaco\"; ");
     loginLayout.getChildren().add(login);
-    Image icon = new Image(
-        "file:src\\util\\gui\\icon\\Company.png");
     // primaryStage.initStyle(StageStyle.UTILITY);
-    primaryStage.getIcons().add(icon);
+    primaryStage.getIcons().add(new Image(
+        "file:src\\util\\gui\\icon\\Company.png"));
     primaryStage.setScene(loginScene);
     primaryStage.setTitle("Digi inc.");
     primaryStage.setMinWidth(500);
@@ -41,6 +40,7 @@ public class GUI extends Application {
     primaryStage.setMaxHeight(Screen.getPrimary().getBounds().getHeight());
     primaryStage.setMaxWidth(Screen.getPrimary().getBounds().getWidth());
     primaryStage.show();
+
   }
 
   public static void main(String[] args) {
--- src/util/gui/ModifiedScene.java
+++ src/util/gui/ModifiedScene.java
@@ -19,15 +19,15 @@ public class ModifiedScene extends Scene {
   pane.getChildren().add(text);
   improvePaneContent.accept(pane, textFields);
   pane.setAlignment(Pos.CENTER);
-  text.setStyle("-fx-fill: #F5F5E9; -fx-font: 30 \"Courier New\"; -fx-font-weight: bold");
+  text.setStyle("-fx-fill: #F5F5E9; -fx-font: 50 \"Courier New\"; -fx-font-weight: bold");
   text.setStroke(Color.GRAY);
  }
 
  BiConsumer<VBox, TextField[]> improvePaneContent = (pane, textFields) -> {
   Arrays.stream(textFields).forEach((textField) -> {
-   textField.setFocusTraversable(false);
-   textField.setMaxHeight(60);
-   textField.setMaxWidth(200);
+   // textField.setFocusTraversable(false);
+   textField.setMaxHeight(120);
+   textField.setMaxWidth(300);
   });
   pane.getChildren().addAll(textFields);
  };
