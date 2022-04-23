9b55860d09960f72f84a49e51ff1aa3da1975975
DigitalRedPanda <osama-12341905@hotmail.com>
2022-04-23 13:16:38 +0300

    Improving GUI

--- src/util/gui/GUI.java
+++ src/util/gui/GUI.java
@@ -1,5 +1,11 @@
 package util.gui;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+
 import javafx.animation.TranslateTransition;
 
 import javafx.application.Application;
@@ -7,7 +13,8 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 
 import javafx.geometry.Pos;
-
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.scene.control.TextField;
@@ -15,7 +22,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 import javafx.scene.paint.Color;
@@ -32,64 +39,42 @@ public class GUI extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-
     TranslateTransition translate = new TranslateTransition();
-
-    // BorderPane border = new BorderPane();
-
-    VBox loginLayout = new VBox(10);
-
-    // border.getChildren().add(loginLayout);
-
+    VBox loginLayout = new VBox(5);
     Scene scene = new Scene(loginLayout, 600, 600/* , Color.valueOf("#142850") */);
-
-    scene.setFill(Color.valueOf("#142850"));
-
-    Text txt1 = new Text("Username");
-
-    // BorderPane.setAlignment(loginLayout, Pos.BASELINE_CENTER);
-
+    TextField txtf1 = new TextField();
+    txtf1.setPromptText("Enter Username");
+    TextField txtf2 = new TextField();
+    txtf2.setPromptText("Enter passowrd");
+    Text txt1 = new Text("Login");
     txt1.setStyle("-fx-fill: #F5F5E9; -fx-font: 30 \"Courier New\";");
-
     txt1.setStroke(Color.GRAY);
-
-    /*
-     * TextField txtf1 = new TextField("Enter your name");
-     * 
-     * txtf1.setMaxWidth(150);
-     */
-    loginLayout.setPadding(new Insets(10, 0, 10, 0));
-
-    loginLayout.getChildren().addAll(txt1/* , txtf1 */);
-
+    loginLayout.getChildren().add(txt1);
+    improvePaneContent.accept(loginLayout, new TextField[] { txtf1, txtf2 });
+    scene.setFill(Color.valueOf("#142850"));
     loginLayout.setAlignment(Pos.CENTER);
-
     Image icon = new Image(
         "file:src\\util\\gui\\icon\\Company.png");
-
     primaryStage.initStyle(StageStyle.DECORATED);
-
     primaryStage.getIcons().add(icon);
-
     primaryStage.setScene(scene);
-
-    primaryStage.setTitle("Registration");
-
+    primaryStage.setTitle("Login");
     primaryStage.setMinWidth(500);
-
     primaryStage.setMinHeight(600);
-
     primaryStage.setMaxHeight(Screen.getPrimary().getBounds().getHeight());
-
     primaryStage.setMaxWidth(Screen.getPrimary().getBounds().getWidth());
-
     primaryStage.show();
-
   }
 
+  BiConsumer<Pane, TextField[]> improvePaneContent = (pane, textFields) -> {
+    Arrays.stream(textFields).forEach((textField) -> {
+      textField.setMaxHeight(60);
+      textField.setMaxWidth(200);
+    });
+    pane.getChildren().addAll(textFields);
+  };
+
   public static void main(String[] args) {
-
     launch();
-
   }
 }
