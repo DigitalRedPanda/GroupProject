59888665a6215eed800f1a0b730e4a3933d0c62a
DigitalRedPanda <osama-12341905@hotmail.com>
2022-05-06 08:35:49 +0300

    Applying the SOLID principles

--- src/company/member/Member.java
+++ src/company/member/Member.java
@@ -1,11 +1,6 @@
 package company.member;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.Date;
-import java.util.Optional;
-import java.util.Scanner;
-import java.util.function.Predicate;
 
 public abstract class Member {
 
@@ -31,25 +26,6 @@ public abstract class Member {
 
     }
 
-    boolean infoExists(String contentToSearch, int position) {
-
-        try (Scanner scan = new Scanner(new File("src/company/member/data/Users.txt"))) {
-            while (scan.hasNextLine()) {
-                var memberInfo = scan.nextLine().split(", ");
-                Optional<String> contentOptional = Optional.of(contentToSearch);
-                contentOptional.filter(content -> content.equals(memberInfo[position])).map(name -> {
-                    return true;
-                });
-                contentOptional.filter(content -> !content.equals(memberInfo[position])).map(name -> {
-                    return false;
-                });
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
     public void setEmail(String email) {
         this.email = email;
     }
@@ -66,7 +42,4 @@ public abstract class Member {
         return email;
     }
 
-    public abstract boolean usernameExists(String memberName);
-
-    public abstract boolean emailExists(String email);
 }
