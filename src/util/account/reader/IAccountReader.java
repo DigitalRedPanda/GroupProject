bc39ad070b08e6294a39c3d25b3f551e23a6b15d
DigitalRedPanda <osama-12341905@hotmail.com>
2022-05-10 09:16:18 +0300

    Merging the Member class with account to lessen size and apply SOLID principles better

--- src/util/account/reader/IAccountReader.java
+++ src/util/account/reader/IAccountReader.java
@@ -3,7 +3,8 @@ package util.account.reader;
 import util.account.Account;
 
 public interface IAccountReader<T extends Account> {
-
+ // these methods are abstract methods that will be implemented inside
+ // AccountReader
  void read(Account account);
 
  boolean isStored(Account account);
